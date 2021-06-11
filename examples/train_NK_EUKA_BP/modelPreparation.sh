6ae2a89763c7b2b913a7007bba14a25aeeb19200
gamerino <merino.gabriela33@gmail.com>
2021-06-11 17:15:19 -0300

    Uploading modelPreparation.sh

--- examples/train_NK_EUKA_BP/modelPreparation.sh
+++ examples/train_NK_EUKA_BP/modelPreparation.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget https://sourceforge.net/projects/deeprotgo/files/examples/train_NK_Euka_BP/DeeProtGO_PSD_Emb_Taxon_Euka_BP_NK.pt --quiet
-mv *.pt DeeProtGO/examples/train_NK_Euka_BP/
+mv *.pt DeeProtGO/examples/train_NK_EUKA_BP/
