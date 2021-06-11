07683c3c4794d4ca9e66044a1c53627f82e01791
gamerino <merino.gabriela33@gmail.com>
2021-06-11 12:36:00 -0300

    uploading processed files

--- data/processed/Training/dataPreparation.sh
+++ data/processed/Training/dataPreparation.sh
@@ -3,4 +3,3 @@ wget https://sourceforge.net/projects/deeprotgo/files/data/processed/Training/Le
 mv LevSim_BP_Euka.h5 DeeProtGO/data/processed/Training/
 tar -xzvf DeeProtGO/data/processed/Training/Emb_BP_Euka.h5.tar.gz 
 tar -xzvf DeeProtGO/data/processed/Training/netOut_BP_Euka.h5.tar.gz 
-
