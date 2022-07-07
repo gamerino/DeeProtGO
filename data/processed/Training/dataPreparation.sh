38c24872b676038c97faf5db477a0b2e425fff93
gmerino <gmerino@ebi.ac.uk>
2022-07-07 09:41:59 +0100

    Uploading NK EUKA CC files

--- data/processed/Training/dataPreparation.sh
+++ data/processed/Training/dataPreparation.sh
@@ -2,4 +2,7 @@
 wget https://sourceforge.net/projects/deeprotgo/files/data/processed/Training/LevSim_BP_Euka.h5 --quiet
 tar -xzvf DeeProtGO/data/processed/Training/Emb_BP_Euka.h5.tar.gz 
 tar -xzvf DeeProtGO/data/processed/Training/netOut_BP_Euka.h5.tar.gz 
+tar -xzvf DeeProtGO/data/processed/Training/Emb_CC_Euka.h5.tar.gz
+tar -xzvf DeeProtGO/data/processed/Training/netOut_CC_Euka.h5.tar.gz
+
 mv *.h5 DeeProtGO/data/processed/Training/
