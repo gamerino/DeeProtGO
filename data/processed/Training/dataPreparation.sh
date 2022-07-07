786f0fa1a275a35b2af840d0af012bf74e2459cb
gmerino <gmerino@ebi.ac.uk>
2022-07-07 18:26:50 +0100

    Update dataPreparation.sh

--- data/processed/Training/dataPreparation.sh
+++ data/processed/Training/dataPreparation.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 wget https://sourceforge.net/projects/deeprotgo/files/data/processed/Training/LevSim_BP_Euka.h5 --quiet
+wget https://sourceforge.net/projects/deeprotgo/files/examples/train_NK_Euka_CC/DeeProtGO_PSD_Emb_Taxon_Euka_CC_NK.pt --quiet
+mkdir DeeProtGO/examples/train_NK_Euka_CC
+Mv *.pt DeeProtGO/examples/train_NK_Euka_CC/
+
 tar -xzvf DeeProtGO/data/processed/Training/Emb_BP_Euka.h5.tar.gz 
 tar -xzvf DeeProtGO/data/processed/Training/netOut_BP_Euka.h5.tar.gz 
 tar -xzvf DeeProtGO/data/processed/Training/Emb_CC_Euka.h5.tar.gz
